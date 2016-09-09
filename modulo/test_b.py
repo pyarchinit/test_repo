25d192735e342020fed2592a33c06ccd30b93b46
Luca Mandolesi <pyarchinit@gmail.com>
2016-09-09 16:00:42 +0200

    seconda aggiunta

--- modulo/test_b.py
+++ modulo/test_b.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
+#seconda aggiunta dopo la giunta di guarino
 """questo e un file di prova"""
 
 
@@ -10,4 +10,14 @@ def print_all(p):
 
 
 var = 'gigi'
+print_all(var)
+
+
+#seconda aggiunta dopo la giunta di guarino
+
+def print_all(p):
+	print "dida: " + str(p)
+
+
+var = 'pippo'
 print_all(var)
